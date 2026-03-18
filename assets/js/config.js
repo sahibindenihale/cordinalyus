023d268547bd826a41968603fa39681189fa5bb3
sahibindenihale <aviserent@gmail.com>
2026-03-18 14:05:40 +0300

    Update config.js
    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 window.SITE_CONFIG = {
   companyName: 'Cordinalyus Rent Filo',
-  phoneDisplay: '0501 687 42 34',
+  phoneDisplay: '0501 592 61 05',
   phoneRaw: '95015926105',
   whatsappRaw: '905305147143',
   whatsappMessage: 'Merhaba, Cordinalyus Rent Filo için araç rezervasyonu yapmak istiyorum.',
